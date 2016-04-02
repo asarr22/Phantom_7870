2a3494267aaba9c677ed891073d6bd5220bfcde4
franciscofranco <franciscofranco.1990@gmail.com>
2016-04-02 16:28:48 +0000

    cpufreq: interactive: don't scale up if load is <= 5%, it's not worth it
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: XileForce <rdaguiar360@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -134,6 +134,8 @@ struct cpufreq_interactive_tunables {
 	unsigned int *policy;
 };
 
+#define DOWN_LOW_LOAD_THRESHOLD 5
+
 /* For cases where we have single governor instance for system */
 static struct cpufreq_interactive_tunables *common_tunables;
 static struct cpufreq_interactive_tunables *tuned_parameters[NR_CPUS] = {NULL, };
@@ -426,6 +428,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 			if (new_freq < tunables->hispeed_freq)
 				new_freq = tunables->hispeed_freq;
 		}
+	} else if (cpu_load <= DOWN_LOW_LOAD_THRESHOLD) {
+		new_freq = pcpu->policy->cpuinfo.min_freq;
 	} else {
 		new_freq = choose_freq(pcpu, loadadjfreq);
 		if (new_freq > tunables->hispeed_freq &&
