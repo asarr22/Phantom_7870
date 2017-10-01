b940d9589f939c658648db5a50f5be3905e1390d
Hybridmax <hybridmax95@gmail.com>
2017-10-01 02:50:00 +0200

    sec_battery.c: STORE_MODE: create sysfs to control MIN & MAX battery capacity charge levels
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -17,6 +17,14 @@
 #define SEC_INPUT_VOLTAGE_5V	5
 #define SEC_INPUT_VOLTAGE_9V	9
 
+static unsigned int STORE_MODE_CHARGING_MAX = 90;
+static unsigned int STORE_MODE_CHARGING_MIN = 20;
+
+module_param_named(store_mode_max, STORE_MODE_CHARGING_MAX, uint, S_IWUSR | S_IRUGO);
+module_param_named(store_mode_min, STORE_MODE_CHARGING_MIN, uint, S_IWUSR | S_IRUGO);
+
+const char *charger_chip_name;
+
 bool sleep_mode = false;
 
 bool slate_mode_state;
