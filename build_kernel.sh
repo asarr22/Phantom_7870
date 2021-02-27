8a63731e9c86cdd2a66b6ef457d9e5b6f94ae1d9
asarr22 <msarr3334@gmail.com>
2021-02-27 20:14:01 +0000

    config: Adjusting

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,6 @@ export CROSS_COMPILE=/home/runner/asarre/linaro-toolchain-latest/bin/aarch64-lin
 export ANDROID_MAJOR_VERSION=q
 export ANDROID_PLATFORM_VERSION=10
 
-make O=./out $1
-make O=./out -j64
+make exynos7870-j7y17lte_defconfig
+
+make -j64
